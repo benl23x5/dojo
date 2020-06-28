3edf89b7783deed36b40d33767802ab2f1f1bb54
Ben Lippmeier <benl@ouroborus.net>
2020-06-28 11:43:15 +0000

    event edit: disambiguate people based on name prefixes

--- src/dojo/Dojo/Data/Person/Search.hs
+++ src/dojo/Dojo/Data/Person/Search.hs
@@ -6,8 +6,9 @@ where
 import Dojo.Data.Person.Database
 import Dojo.Data.Person.Base
 import Dojo.Base
-import qualified Data.Set       as Set
-
+import Data.Function
+import qualified Data.Char as Char
+import qualified Data.List as List
 
 -- Found ----------------------------------------------------------------------
 -- | Find a person based on their name.
@@ -23,73 +24,68 @@ data Found a
         deriving Show
 
 
--- | Apply a function to any value carried in a `Found`
-mapFoundIO  :: (a -> IO b) -> Found a -> IO (Found b)
-mapFoundIO f ff
- = case ff of
-        FoundOk x
-         -> do  x'      <- f x
-                return  $ FoundOk x'
-
-        FoundMany xs
-         -> do  xs'     <- mapM f xs
-                return  $ FoundMany xs'
-
-        FoundNone
-         -> return FoundNone
-
 
 -- Search ---------------------------------------------------------------------
 -- | Find a person based on a search string.
-findPerson
-        :: IConnection conn
-        => conn -> String -> IO (Found Person)
+--
+--   We assume the search string consists of at least one complete name
+--   and some optional initials, eg "john", "john s" or "j smith"
+--
+--   We first find all people that match any of the words in the search
+--   string, then disambiguate them by selecting only people that have a
+--   name with a prefix that satisfies all of the words in the search string.
+--
+--   This sends several queries to the DB to do the search,
+--   but we don't have any user load, so don't worry too much.
+--
+findPerson :: IConnection conn
+           => conn -> String -> IO (Found Person)
 
 findPerson conn strSearch
  = do   let strWords    = words strSearch
 
-        hitssPreferedName
-         <- mapM (searchPeopleFromField conn "PreferedName") strWords
+        psPreferredName
+         <- fmap concat
+         $  mapM (searchPeopleFromField conn "PreferedName") strWords
 
-        hitssFirstName
-         <- mapM (searchPeopleFromField conn "FirstName")    strWords
+        psFirstName
+         <- fmap concat
+         $  mapM (searchPeopleFromField conn "FirstName")    strWords
 
-        hitssFamilyName
-         <- mapM (searchPeopleFromField conn "FamilyName")   strWords
+        psFamilyName
+         <- fmap concat
+         $  mapM (searchPeopleFromField conn "FamilyName")   strWords
 
-        let pidFound     = discriminateResults
-                                hitssPreferedName
-                                hitssFirstName
-                                hitssFamilyName
-
-        mapFoundIO (getPerson conn) pidFound
+        let psAll  = concat [psPreferredName, psFirstName, psFamilyName]
+        return $ discriminateResults strSearch psAll
 
 
-discriminateResults
-        :: [[PersonId]]         -- ^ Hits from prefered names.
-        -> [[PersonId]]         -- ^ Hits from first names.
-        -> [[PersonId]]         -- ^ Hits from family names.
-        -> Found PersonId
+-- | Given a search string and a list of candidate person matches,
+--   try to select a single person by comparing name prefixes.
+discriminateResults :: String -> [Person] -> Found Person
+discriminateResults sQuery psMatch
+ = let psSat
+        = List.nubBy ((==) `on` personId)
+        $ [ person  | person <- psMatch
+                    , all (isWordSatisfied person) $ words sQuery ]
+   in case psSat of
+         []   -> case psMatch of
+                        []   -> FoundNone
+                        [p1] -> FoundOk p1
+                        ps   -> FoundMany ps
+         [p1] -> FoundOk p1
+         ps   -> FoundMany ps
 
-discriminateResults hitsPrefered hitsFirst hitsFamily
- = let  allPids = Set.unions
-                [ Set.unions $ map Set.fromList hitsPrefered
-                , Set.unions $ map Set.fromList hitsFirst
-                , Set.unions $ map Set.fromList hitsFamily ]
 
-        result
-         | Set.size allPids == 0
-         = FoundNone
-
-         | Set.size allPids == 1
-         = let  [pid]   = Set.toList allPids
-           in   FoundOk pid
-
-         -- TODO: discriminate based on names.
-         | otherwise
-         = FoundMany (Set.toList allPids)
-
-   in result
+-- | Check if a search keyword is satisfied by a person.
+--   This is true if the search keyword is a prefix of any of the person's names.
+isWordSatisfied :: Person -> String -> Bool
+isWordSatisfied person sWord
+ = let  sWord' = map Char.toLower sWord
+        sName (PersonName s) = map Char.toLower s
+   in   any (isPrefixOf sWord') (words $ sName $ personFirstName person)
+     || any (isPrefixOf sWord') (words $ sName $ personPreferredName person)
+     || any (isPrefixOf sWord') (words $ sName $ personFamilyName person)
 
 
 -- | Get the list of personIds that have the given value for a field.
@@ -97,22 +93,23 @@ discriminateResults hitsPrefered hitsFirst hitsFamily
 --   Hard coded limit to 5 results.
 searchPeopleFromField
         :: IConnection conn
-        => conn -> String -> String -> IO [PersonId]
+        => conn -> String -> String -> IO [Person]
 
 searchPeopleFromField conn fieldName name
-        = liftM convertResult
-        $ quickQuery' conn (unlines
-                [ "SELECT PersonId"
-                , "FROM   Person"
-                , "WHERE  UPPER(" ++ fieldName ++ ")=UPPER(?)"
-                , "LIMIT  5"])
-                [toSql name]
+ = liftM convertResult
+ $ quickQuery' conn (unlines
+        [ "SELECT "
+        , "PersonId,MemberId,"
+        , "PreferedName,FirstName,MiddleName,FamilyName,"
+        , "DateOfBirth,Mobile,Email"
+        , "FROM   Person"
+        , "WHERE  UPPER(" ++ fieldName ++ ")=UPPER(?)"
+        , "LIMIT  5"])
+        [toSql name]
 
 
-convertResult :: [[SqlValue]] -> [PersonId]
-convertResult valuess
- = map (\vs -> case vs of
-                [spid]  -> fromSql spid
-                _       -> error "convertResult: unexpected result") valuess
+convertResult :: [[SqlValue]] -> [Person]
+convertResult values
+ = map personOfSqlValues values
 
 
